a505264b48572d0493841f657f2269ad72c66bed
Raquel Sousa <Raquelmcoelhodesousa@gmail.com>
2023-11-22 18:34:39 -0300

    feat: kill rat

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,12 +1,19 @@
-extends Area2D
+extends StaticBody2D
 class_name Enemy
 
+func _ready():
+	$AnimatedSprite2D.play("front")
 
-func _on_Aread2D_body_entered(body):
-	if body.get_name() == "Player":
-		die()
+func _process(_delta):
+	$AnimatedSprite2D.play("side")
 		
 func die():
 	# emitir som de morte
 	Main.coins += Main.bonus
 	queue_free()
+
+func _on_body_entered(body):
+	print("entrou")
+	print(body.get_name())
+	if body.get_name() == "Player":
+		die()
