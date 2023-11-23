c41a7ce58fb8637ac4ceb2ce3c4bdfc9c7a5743e
Raquel Sousa <Raquelmcoelhodesousa@gmail.com>
2023-11-23 11:09:47 -0300

    feat: costumers line

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -3,17 +3,20 @@ extends Node
 var coin_scene = load("res://Scenes/Coin.tscn")
 var rat_scene = load("res://Scenes/Rat.tscn")
 var food_scene = load("res://Scenes/Food.tscn")
+var costumer_scene = load("res://Scenes/Costumer.tscn")
 var bonus = 10
 var coins = 0
 var is_dragging = false
 
+func _ready():
+	spawn_clients()
+	
 func receiveMoney():
 	pass
 
 func spawnEnemy():
-	var rat = rat_scene.instantiate()
-	add_child(rat)
-
+	pass
+	
 func spawn_food(station):
 	var food = food_scene.instantiate()
 	food.sprite_str = "pasta"
@@ -23,3 +26,13 @@ func spawn_food(station):
 func _on_enemy_timer_timeout():
 	pass
 	#spawnEnemy()
+	
+	
+func spawn_clients():
+	var line = []
+	for i in range(5):
+		print("cliente ", i)
+		var costumer = costumer_scene.instantiate()
+		costumer.position_in_line = i
+		line.append(costumer)
+		add_child(costumer)
