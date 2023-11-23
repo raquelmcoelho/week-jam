780f43ce4081e73fc9a8974016c8e8ec504b28ff
Raquel Sousa <Raquelmcoelhodesousa@gmail.com>
2023-11-23 17:11:08 -0300

    chore: randomizes clients

--- Scripts/Costumer.gd
+++ Scripts/Costumer.gd
@@ -1,14 +1,15 @@
 extends CharacterBody2D
 
 var position_in_line = 0;
-var order = 0
+var order = ""
 var options = ["pasta", "sandwich", "soup"]
 var skins = [
 	"cartola", "laco", "paulin", "bone", "bigodon"
 ]
 
 func _ready():
-	$AnimatedSprite2D.play(skins[position_in_line])
+	$AnimatedSprite2D.play(skins[randi_range(0,4)])
+	order = options[randi_range(0,2)]
 
 func _physics_process(_delta):
 	adjust_position_line()
@@ -17,6 +18,6 @@ func adjust_position_line():
 	var place_offset = 80
 	var screen_size = get_viewport().get_visible_rect().size
 	var x_position = 0.7 * screen_size.x + (position_in_line * place_offset)
-	var y_position = 0.7 * screen_size.y
+	var y_position = 0.3 * screen_size.y
 	self.position = Vector2(x_position, y_position)
 	
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -6,6 +6,7 @@ var food_scene = load("res://Scenes/Food.tscn")
 var costumer_scene = load("res://Scenes/Costumer.tscn")
 var barrel_scene = load("res://Scenes/Barrel.tscn")
 
+var line = []
 var bonus = 10
 var coins = 0
 var is_dragging = false
@@ -47,7 +48,6 @@ func spawn_barrel(sprite, position):
 	add_child(barrel)
 
 func spawn_clients():
-	var line = []
 	for i in range(5):
 		print("cliente ", i)
 		var costumer = costumer_scene.instantiate()
